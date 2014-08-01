8496e88d76df4cbb21a5bf1fc3df65a8afb0df5e
Hawk Weisman <hawk.weisman@gmail.com>
2014-08-01 11:45:41 -0400

    FileHandle now lives in io package as God intended.

--- src/com/meteorcode/pathway/io/FileHandle.scala
+++ src/com/meteorcode/pathway/io/FileHandle.scala
@@ -1,4 +1,4 @@
-
+package com.meteorcode.pathway.io
 import java.io.{
   File,
   FileInputStream,
--- src/com/meteorcode/pathway/io/ResourceManager.scala
+++ src/com/meteorcode/pathway/io/ResourceManager.scala
@@ -1,7 +1,9 @@
 package com.meteorcode.pathway.io
 
+import com.meteorcode.pathway.io.FileHandle
 
-object ResourceManager {
+class ResourceManager (assetsDir: String) {
+  
   def read (path: String): FileHandle = {
     //TODO: Stub
     return new DesktopFileHandle(path)
