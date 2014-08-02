7387c2e6f7d6abb7184435983bd9d0ba430adabe
Hawk Weisman <hawk.weisman@gmail.com>
2014-08-02 18:00:01 -0400

    `ResourceManager` no longer calls `FileHandle.apply()` method removed in a6cc1ef

--- src/com/meteorcode/pathway/io/ResourceManager.scala
+++ src/com/meteorcode/pathway/io/ResourceManager.scala
@@ -3,6 +3,6 @@ package com.meteorcode.pathway.io
 import com.meteorcode.pathway.io.FileHandle
 
 class ResourceManager (private val assetsDir: FileHandle) {
-  def this() = this(FileHandle("assets"))
+  def this() = this(new DesktopFileHandle("assets"))
   // TODO: traverse the tree from the initial FileHandle down and call list(), building the tree?
 }
