4a7eb1859b0cfb859380a947f766f64b009081c5
Hawk Weisman <hawk.weisman@gmail.com>
2014-08-03 12:40:21 -0400

    Fix "permanently hidden" warning from scalac

--- src/com/meteorcode/pathway/io/ResourceManager.scala
+++ src/com/meteorcode/pathway/io/ResourceManager.scala
@@ -1,7 +1,5 @@
 package com.meteorcode.pathway.io
 
-import com.meteorcode.pathway.io.FileHandle
-
 class ResourceManager (private val assetsDir: FileHandle) {
   def this() = this(new DesktopFileHandle("assets"))
   // TODO: traverse the tree from the initial FileHandle down and call list(), building the tree?
