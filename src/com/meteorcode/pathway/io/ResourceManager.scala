345b19b495e66f4476534726ee8b81b6a6d20480
Hawk Weisman <hawk.weisman@gmail.com>
2014-08-03 12:55:10 -0400

    Hid FileHandle subclasses in a companion object so that nobody would call their constructors by mistake; the Correct Way to get a new FileHandle is by calling FileHandle(path) if you're Scala or FileHandle.handle(path) from Java

--- src/com/meteorcode/pathway/io/ResourceManager.scala
+++ src/com/meteorcode/pathway/io/ResourceManager.scala
@@ -1,6 +1,6 @@
 package com.meteorcode.pathway.io
 
 class ResourceManager (private val assetsDir: FileHandle) {
-  def this() = this(new DesktopFileHandle("assets"))
+  def this() = this(FileHandle("assets"))
   // TODO: traverse the tree from the initial FileHandle down and call list(), building the tree?
 }
