4109503c4dc7ee5f31675843bd2a4a6f81f8bb3a
Hawk Weisman <hawk.weisman@gmail.com>
2014-08-03 13:21:35 -0400

    Documented changes from 345b19b in JavaDoc.

--- src/com/meteorcode/pathway/io/FileHandle.scala
+++ src/com/meteorcode/pathway/io/FileHandle.scala
@@ -45,6 +45,10 @@ import scala.collection.JavaConversions._
  * <p> All FileHandles are backed by {@link java.io.InputStream InputStream}s and {@link java.io.OutputStream OutputStream}s,
  * so reading and writing to a file should be consistent across operating systems, filesystem locations, and file types.</p>
  *
+ * <p>DO NOT use the FileHandle() constructor if you want a FileHandle into a file. Instead, get it from
+ * {@link com.meteorcode.pathway.io.FileHandle#apply apply(path)}  (call FileHandle(path)) from Scala, or
+ * {@link com.meteorcode.pathway.io.FileHandle#handle handle(path)} from Java. Calling new FileHandle() will NOT give you what you want.</p>
+ *
  * @author Hawk Weisman
  */
 abstract class FileHandle {
@@ -268,7 +272,7 @@ object FileHandle {
 
   /**
    * <p>A FileHandle into a file or directory within a zip archive.</p>
-   * <p>DON'T MAKE THESE - if you want to handle a file, please get it from an instance of {@link com.meteorcode.pathway.io.ResourceManager ResourceManager}.
+   * <p>DON'T MAKE THESE - if you want to handle a file, please get it from {@link com.meteorcode.pathway.io.FileHandle#apply apply()}.
    * The FileHandle system is supposed to allow you to treat files in zip/jar archives as though they were on the filesystem as regular files, but this only works
    * if you treat all files you have to access as instances of {@link com.meteorcode.pathwaawy.io.FileHandle FileHandle}. If you ever refer to files as
    * DesktopFileHandle, ZipFileHandle, or JarFileHandle explicitly in your code, you are doing the Wrong Thing and negating a whole lot of time and effort I
