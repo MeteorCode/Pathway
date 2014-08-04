578e6f328b1f8eefee1d922a275d8932e8332f9e
Max Clive <mattrulz127@gmail.com>
2014-08-04 13:41:15 -0400

    Two of three tests pass.

--- src/com/meteorcode/pathway/test/SimpleIOIntegrationTest.java
+++ src/com/meteorcode/pathway/test/SimpleIOIntegrationTest.java
@@ -3,7 +3,7 @@
 // THIS TEST FAILS ON JENKINS                   //
 // Un-comment it and run it locally to confirm  //
 // that it works as expected, but DO NOT commit //
-//////////////////////////////////////////////////
+////////////////////////////////////////////////*/
 package com.meteorcode.pathway.test;
 
 import static org.junit.Assert.*;
@@ -16,30 +16,29 @@ import java.io.IOException;
 /**
  * Non-comprehensive test case to assert that the IO package does the Right Thing
  * THIS IS NOT A UNIT TEST - io classes should be unit tested for coverage as well.
- *//*
+ */
 public class SimpleIOIntegrationTest {
     private FileHandle underTest;
 
 	@Test
 	public void testUnzippedFileHandle() throws IOException {
-        underTest = FileHandle.handle("src/test/resources/test1.txt");
-        assertEquals("hi!", underTest.readString());
+	    underTest = ClasspathTestShimDELETEME.c("/test/resources/test1.txt");
+	    assertEquals("hi!", underTest.readString());
 	}
 
     @Test
     public void testZippedFileHandle() throws IOException {
-        underTest = FileHandle.handle("src/test/resources/zippedtest.zip");
+        underTest = ClasspathTestShimDELETEME.c("/test/resources/zippedtest.zip");
         assertTrue(underTest.isDirectory());
         assertEquals(underTest.list().get(0).readString(), "also hi!");
     }
 
     @Test
     public void testDirFileHandle() throws IOException{
-        underTest = FileHandle.handle("src/test/resources/testDir");
+        underTest = ClasspathTestShimDELETEME.c("/test/resources/testDir");
         assertTrue(underTest.isDirectory());
         assertEquals("yet again hi", underTest.list().get(0).readString());
         assertEquals("still hi", underTest.list().get(1).readString());
     }
 
 }
-*/
