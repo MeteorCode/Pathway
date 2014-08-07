23696001c9ed1dec7906bbcbe941e18cf116b0c1
Hawk Weisman <hawk@meteorcodelabs.com>
2014-08-07 00:13:28 -0400

    Add helper methods for getting name and extension to FileHandle.

--- src/com/meteorcode/pathway/io/FileHandle.scala
+++ src/com/meteorcode/pathway/io/FileHandle.scala
@@ -1,10 +1,11 @@
 package com.meteorcode.pathway.io
+
 import java.io.{
-  InputStream,
-  OutputStream,
-  BufferedOutputStream,
-  BufferedInputStream,
-  IOException
+InputStream,
+OutputStream,
+BufferedOutputStream,
+BufferedInputStream,
+IOException
 }
 import java.util
 import java.nio.charset.Charset
@@ -44,65 +45,80 @@ abstract class FileHandle(protected[io] val manager: ResourceManager) {
   def writable: Boolean
 
   /** <p>Returns the path to this FileHandle.</p>
-   * <p>All paths are treated as into Unix-style paths for cross-platform purposes.
-   */
+    * <p>All paths are treated as into Unix-style paths for cross-platform purposes.
+    */
   def path: String
 
   /**
+   * @return The filename extension of this string
+   */
+  def extension: String = path.split('.').drop(1).lastOption match {
+    case s:Some => s.get
+    case None => ""
+  }
+
+  /**
+   * @return the name of this object, without the filename extension
+   */
+  def name: String = path.split('/').getLast.split('.').getFirst
+
+  /**
    * Returns a {@link java.io.File java.io.File} that represents this file handle.
-   * @return a {@link java.io.File java.io.File} that represents this file handle, or null if this file is inside a Jar or Zip archive.
+   * @return a { @link java.io.File java.io.File} that represents this file handle, or null if this file is inside a Jar or Zip archive.
    */
   def file: java.io.File
 
   /**
-   *  <p>Returns a list containing FileHandles to the contents of FileHandle .</p>
-   *  <p> Returns an empty list if this file is not a directory or does not have contents.</p>
+   * <p>Returns a list containing FileHandles to the contents of FileHandle .</p>
+   * <p> Returns an empty list if this file is not a directory or does not have contents.</p>
    */
   def list: util.List[FileHandle]
 
   /**
-   *  <p>Returns a list containing FileHandles to the contents of this FileHandle with the specified suffix.</p>
-   *  <p> Returns an empty list if this file is not a directory or does not have contents.</p>
-   *  @param suffix
+   * <p>Returns a list containing FileHandles to the contents of this FileHandle with the specified suffix.</p>
+   * <p> Returns an empty list if this file is not a directory or does not have contents.</p>
+   * @param suffix
    */
   def list(suffix: String): util.List[FileHandle] = list.filter(entry => entry.path.endsWith(suffix))
 
   /** Returns a stream for reading this file as bytes.
-   *  @throws IOException if the file does not exist or is a directory.
-   */
+    * @throws IOException if the file does not exist or is a directory.
+    */
   @throws(classOf[IOException])
   def read: InputStream
 
   /** Returns a buffered stream for reading this file as bytes.
-   *  @throws IOException if the file does not exist or is a directory.
-   */
+    * @throws IOException if the file does not exist or is a directory.
+    */
   @throws(classOf[IOException])
-  def read(bufferSize: Integer): BufferedInputStream = new BufferedInputStream (read, bufferSize)
+  def read(bufferSize: Integer): BufferedInputStream = new BufferedInputStream(read, bufferSize)
 
   /** Reads the entire file into a string using the platform's default charset.
-	 *  @throws IOException if the file does not exist or is a directory.
-   */
+    * @throws IOException if the file does not exist or is a directory.
+    */
   @throws(classOf[IOException])
   def readString: String = readString(Charset.defaultCharset())
 
-  /** Reads the entire file into a string using the specified charset.*/
+  /** Reads the entire file into a string using the specified charset. */
   @throws(classOf[IOException])
   def readString(charset: Charset): String = Source.fromInputStream(read).mkString
 
   /** Returns an {@link java.io.OutputStream OutputStream} for writing to this file.
-   * @return an {@link java.io.OutputStream OutputStream} for writing to this file, or null if this file is not writeable.
-   * @param append If false, this file will be overwritten if it exists, otherwise it will be appended.
-   */
-   def write(append: Boolean): OutputStream
+    * @return an { @link java.io.OutputStream OutputStream} for writing to this file, or null if this file is not writeable.
+    * @param append If false, this file will be overwritten if it exists, otherwise it will be appended.
+    */
+  def write(append: Boolean): OutputStream
 
   /** Returns an {@link java.io.BufferedOutputStream BufferedOutputStream} for writing to this file.
-   * @return an {@link java.io.BufferedOutputStream BufferedOutputStream} for writing to this file, or null if this file is not writeable.
-   * @param bufferSize The size of the buffer
-   * @param append If false, this file will be overwritten if it exists, otherwise it will be appended.
-   */
-   def write(bufferSize: Integer, append: Boolean): BufferedOutputStream = if (writable) { new BufferedOutputStream(write(append), bufferSize) } else null
+    * @return an { @link java.io.BufferedOutputStream BufferedOutputStream} for writing to this file, or null if this file is not writeable.
+    * @param bufferSize The size of the buffer
+    * @param append If false, this file will be overwritten if it exists, otherwise it will be appended.
+    */
+  def write(bufferSize: Integer, append: Boolean): BufferedOutputStream = if (writable) {
+    new BufferedOutputStream(write(append), bufferSize)
+  } else null
 
-   /** <p>Writes the specified string to the file using the default charset.</p>
+  /** <p>Writes the specified string to the file using the default charset.</p>
     * <p>Throws an {@link java.io.IOException IOException} if the FileHandle represents something that is not writeable; yes, I am aware
     * that having some of these methods return null and others throw exceptions is Wrong and I should feel Bad,
     * I wanted them to return an {@link scala.Option Option}, but Max wouldn't let me.</p>
@@ -110,20 +126,28 @@ abstract class FileHandle(protected[io] val manager: ResourceManager) {
     * @param append If false, this file will be overwritten if it exists, otherwise it will be appended.
     * @throws IOException if this file is not writeable
     */
-   @throws(classOf[IOException])
-   def writeString (string: String, append: Boolean): Unit = if (writable) { writeString(string, Charset.defaultCharset(), append) } else { throw new IOException("FileHandle " + path + " is not writeable.") }
+  @throws(classOf[IOException])
+  def writeString(string: String, append: Boolean): Unit = if (writable) {
+    writeString(string, Charset.defaultCharset(), append)
+  } else {
+    throw new IOException("FileHandle " + path + " is not writeable.")
+  }
 
-   /** Writes the specified string to the file using the specified  charset.
+  /** Writes the specified string to the file using the specified  charset.
     * <p>Throws an {@link java.io.IOException IOException} if the FileHandle represents something that is not writeable; yes, I am aware
     * that having some of these methods return null and others throw exceptions is Wrong and I should feel Bad,
     * I wanted them to return an {@link scala.Option Option}, but Max wouldn't let me.</p>
     * @param string the string to write to the file
-    * @param charset the {@link java.nio.charset.Charset charset} to use while writing to the file
+    * @param charset the { @link java.nio.charset.Charset charset} to use while writing to the file
     * @param append If false, this file will be overwritten if it exists, otherwise it will be appended.
     * @throws IOException if this file is not writeable
     */
-   @throws(classOf[IOException])
-   def writeString (string: String, charset: Charset, append: Boolean): Unit = if (writable) { write(append).write(string.getBytes(charset)) } else { throw new IOException("FileHandle " + path + " is not writeable.") }
+  @throws(classOf[IOException])
+  def writeString(string: String, charset: Charset, append: Boolean): Unit = if (writable) {
+    write(append).write(string.getBytes(charset))
+  } else {
+    throw new IOException("FileHandle " + path + " is not writeable.")
+  }
 
-   override def toString = path
+  override def toString = path
 }
