4a1239c06ef8fbb61653de8d57caffd2239e42bd
Hawk Weisman <hawk@meteorcodelabs.com>
2014-08-07 00:13:02 -0400

    Now featuring recursive walk

--- src/com/meteorcode/pathway/io/ResourceManager.scala
+++ src/com/meteorcode/pathway/io/ResourceManager.scala
@@ -1,9 +1,31 @@
 package com.meteorcode.pathway.io
 
 import java.io.File
+import scala.collection.immutable.HashMap
 
-class ResourceManager (private val assetsDir: String) {
-  def this() = this("assets")
+class ResourceManager (private val directories: List[FileHandle]) {
+  def this(directory: FileHandle) = this(List(directory))
+  def this() = this(List[FileHandle](new DesktopFileHandle("assets")))
+
+  private var cachedPaths = HashMap[String, String]
+
+  directories.foreach{ directory =>
+    def walk(h: FileHandle, currentPath: String) { // recursively walk the directories and cache the paths
+      h.list.foreach { f: FileHandle =>
+        f.extension match {
+          case "jar" | "zip" =>             // special-case jars/zips because we want them to have directory-like paths
+            cachedPaths += f.name -> f.path // add (logical path -> real path) to the cached paths
+            walk(f, f.name)                 // walk all children of this dir
+          case _ =>
+            cachedPaths += currentPath + f.name + f.extension -> f.path
+            walk (f, currentPath)
+          }
+        }
+      }
+    walk(directory, directory.name)
+    }
+  }
+
   // TODO: traverse the tree from the initial FileHandle down and call list(), building the tree?
   private val cachedHandles = collection.mutable.Map[String, FileHandle]()
 
