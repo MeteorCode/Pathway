92ce6b2a0f44e23a1313a0f2f35b381107446be9
Hawk Weisman <hawk@meteorcode.com>
2014-08-07 20:41:35 -0400

    SimpleIOIntegrationTest now fails with messages

--- src/com/meteorcode/pathway/test/SimpleIOIntegrationTest.java
+++ src/com/meteorcode/pathway/test/SimpleIOIntegrationTest.java
@@ -45,17 +45,17 @@ public class SimpleIOIntegrationTest {
     @Test
     public void testZippedFileHandle() throws IOException {
         underTest = r.handle("zippedtest.zip");
-        assertTrue(underTest.isDirectory());
-        assertNull(underTest.read());
-        assertNull(underTest.write(true));
-        assertEquals(underTest.list().get(0).readString(), "also hi!");
+        assertTrue("FAIL: Zip file did not claim to be a directory.", underTest.isDirectory());
+        assertNull("FAIL: Zipfile.read() was not null.", underTest.read());
+        assertNull("FAIL: Zipfile.write() was not null.", underTest.write(true));
+        assertEquals("FAIL: Zipfile did not list expected contents.", underTest.list().get(0).readString(), "also hi!");
     }
 
     @Test
     public void testDirFileHandle() throws IOException {
         underTest = r.handle("testDir");
         assertFalse("FAIL: Directory claimed to be writable.", underTest.writable());
-        assertTrue(underTest.isDirectory());
+        assertTrue("FAIL: Directory claimed not to be.", underTest.isDirectory());
         assertNull("FAIL: directory gave us an OutputStream?", underTest.write(true));
         System.out.println(underTest.list().toString()); // FUCK IT. we can test for correctness manually.
         try {
