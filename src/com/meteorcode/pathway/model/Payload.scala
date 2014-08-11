007a11703dacfa4e13ca02c98f31cb959d78a008
Hawk Weisman <hawk@meteorcodelabs.com>
2014-08-10 22:32:10 -0400

    Revert "Revert "Swap `tryToStamp()` return values""
    This reverts commit 33e625f25b098f1125ef9b4acbf664a6c966e2c9.

--- src/com/meteorcode/pathway/model/Payload.scala
+++ src/com/meteorcode/pathway/model/Payload.scala
@@ -35,11 +35,11 @@ class Payload(initialMap: scala.collection.Map[String, Object], tile: Tile) {
 
   /**
    * Attempts to stamp this Payload with a [[com.meteorcode.model.Property]] if this Payload has not already been
-   * stamped by that Property, and returns true if the Payload has already been stamped.
+   * stamped by that Property, and returns false if the Payload has already been stamped.
    * @param stampedBy the [[com.meteorcode.model.Property]] stamping this event.
-   * @return true if this Payload was already stamped by that Property, false if not
+   * @return false if this Payload was already stamped by that Property, true if not
    */
-  def tryToStamp (stampedBy: Property) = if (stampExists(stampedBy)) { true } else{ stamp(stampedBy); false }
+  def tryToStamp (stampedBy: Property) = if (stampExists(stampedBy)) { false } else { stamp(stampedBy); true }
   /**
    * Checks to see if this Payload has been stamped by a [[com.meteorcode.model.Property]], marking that
    * that Property has "seen" this Payload
