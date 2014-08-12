86478079879575bc558447ed7d011058966e1ebc
Hawk Weisman <hawk@meteorcodelabs.com>
2014-08-12 14:47:45 -0400

    Too many nulls! :o

--- src/com/meteorcode/pathway/model/Entity.scala
+++ src/com/meteorcode/pathway/model/Entity.scala
@@ -7,13 +7,13 @@ class Entity(gameID: Long,
              protected var grid: Grid,
              protected var name: String) extends GameObject(gameID) {
 
-  if (this.name = null) this.name = this.getClass.getSimpleName
+  if (this.name == null) this.name = this.getClass.getSimpleName
 
-  def this() = this(null, null, null, null, null)
-  def this(gameID: Long) = this (gameID, null, null, null, null)
+  def this() = this(null, null, null, null)
+  def this(gameID: Long) = this (gameID, null, null, null)
   def this(position: (Int, Int), grid: Grid) = this (null, position, grid, null)
-  def this(name: String) = this(null, null, null, null, name)
-  def this(gameID: Long, name:String) = this (gameID, null, null, null, name)
+  def this(name: String) = this(null, null, null, name)
+  def this(gameID: Long, name:String) = this (gameID, null, null, name)
   def this(position: (Int, Int), grid: Grid, name: String) = this (null, position, location, name)
 
   def setLocation (xy: GridCoordinates) = this.coordinates = (xy.x, xy.y)
