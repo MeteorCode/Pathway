f42d8b648f555746b181b5056a9e214fde250d0e
Hawk Weisman <hawk@meteorcodelabs.com>
2014-08-12 13:53:41 -0400

    Document & clean up Grid

--- src/com/meteorcode/pathway/model/Grid.java
+++ src/com/meteorcode/pathway/model/Grid.java
@@ -1,28 +1,29 @@
 package com.meteorcode.pathway.model;
 
+/**
+ * Represents a 2D grid of {@link com.meteorcode.pathway.model.Tile Tiles}, which may be occupied by
+ * {@link com.meteorcode.pathway.model.Entity Entities}, with an associated
+ * {@link com.meteorcode.pathway.model.Context Context} for evaluating scripts.
+ * @author Hawk Weisman
+ */
 public class Grid {
 
     public Grid(int size, String name) {
         this.grid = new Tile[size][size];
         this.name = name;
-        this.context = new Context(name)
+        this.context = new Context(name);
     }
 
     public Grid (int size) {
         new Grid(size, name = this.getClass().getSimpleName());
     }
 
-	//
 	private Tile[][] grid;
-
-
-
     private String name;
     private Context context;
 
 	/**
 	 * Returns the tile at the specified position.
-	 * @author Hawk Weisman
 	 * @param 	x	the x-position of the target tile
 	 * @param 	y	the y-position of the target tile
 	 * @return the tile at the specified position
@@ -33,7 +34,6 @@ public class Grid {
 
 	/**
 	 * Returns the tile at the specified position.
-	 * @author Hawk Weisman
 	 * @param position a GridCoordinates object containing the coordinates of the targeted position.
 	 * @return the tile at the specified position
 	 */
@@ -43,7 +43,6 @@ public class Grid {
 
 	/**
 	 * Insert a tile at the specified position.
-	 * @author Hawk Weisman
 	 * @param x the x-position of the target tile
 	 * @param y	the y-position of the target tile
 	 * @param tile the Tile to insert at the specified position
@@ -52,14 +51,25 @@ public class Grid {
 		this.grid[x][y] = tile;
 	}
 
+    /**
+     * @return this Grid's name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets this Grid's name
+     * @param name the new name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Get a reference to this Grid's associated {@link com.meteorcode.pathway.model.Context scripting context}.
+     * @return this Grid's associated {@link com.meteorcode.pathway.model.Context scripting context}.
+     */
     public Context getContext() {
         return this.context;
     }
