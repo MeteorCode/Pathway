756f9104149a4c751175df718b9653c0845e4801
Hawk Weisman <hawk@meteorcodelabs.com>
2014-08-13 12:33:28 -0400

    Add AlphabeticLoadPolicy.

---
+++ src/com/meteorcode/pathway/io/AlphabeticLoadPolicy.scala
@@ -0,0 +1,18 @@
+package com.meteorcode.pathway.io
+
+/**
+ * Simple [[com.meteorcode.pathway.io.LoadOrderProvider LoadOrderProvider]] implementation.
+ * Paths are given higher priority based on their alphabetic position (case-insensitive).
+ *
+ * You can use this as an example while writing your own LoadOrderProviders.
+ *
+ * Created by hawk on 8/13/14.
+ */
+class AlphabeticLoadPolicy extends LoadOrderProvider {
+  /**
+   * Takes an unordered set of top-level paths and returns a list of those paths, ordered by load priority.
+   * @param paths a set of Strings representing top-level paths in the physical filesystem.
+   * @return a List of those paths ordered by their load priority
+   */
+  def orderPaths(paths: Set[String]): List[String] = paths.to[List].sortWith(_.toLowerCase < _.toLowerCase)
+}
