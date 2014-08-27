0ca37db18c0ecb320b1aa6dd8ea8df10b077fd6a
Hawk Weisman <hawk@meteorcodelabs.com>
2014-08-27 10:19:34 -0400

    Add documentation to ModLoader

--- src/main/java/com/meteorcode/pathway/io/ModLoader.scala
+++ src/main/java/com/meteorcode/pathway/io/ModLoader.scala
@@ -18,6 +18,12 @@ object ModLoader {
 
   /**
    * Loads all mods in the target directory.
+   *
+   * Mods are loaded by running a BeanShell script called `init.java` contained within the mod's archive or directory.
+   * This script is always executed within a clean BeanShell interpreter. The game's
+   * [[com.meteorcode.pathway.io.ResourceManager ResourceManager]] is bound to a variable called `files` in the
+   * BeanShell script environment.
+   *
    * @param directory A [[com.meteorcode.pathway.io.FileHandle]] representing the mods directory
    * @throws IOException if the mods directory is invalid.
    */
@@ -29,8 +35,8 @@ object ModLoader {
       logger.log("Could not load mods directory " + directory + ", did not exist.")
     // otherwise, get all the jarfiles from the directory and load them
     logger.log("loading mods from " + directory)
-    beanshell injectObject("file",directory.manager)
+    beanshell injectObject("files",directory.manager)
     directory.list("init.java").foreach { initScript => beanshell.eval(initScript) }
-    beanshell removeObject("file") //unset so that next time this is called, a different manager is set
+    beanshell removeObject("files") //unset so that next time this is called, a different manager is set
   }
 }
