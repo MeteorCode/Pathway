6aa39b0a0244215b2fe8cd4bee0754c100e3d152
Hawk Weisman <hawk@meteorcodelabs.com>
2014-10-15 21:37:18 -0400

    Start working on a Scala ClobberTable implementation

---
+++ src/main/java/com/meteorcode/pathway/ForkTable.scala
@@ -0,0 +1,33 @@
+package com.meteorcode.pathway
+
+import scala.collection.mutable
+
+/**
+ * Scala re-implementation of Max's ClobberTable
+ * Created by hawk on 10/15/14.
+ */
+class ForkTable[K, V](val parent: ForkTable[K, V] = null) extends mutable.HashMap[K, V] {
+  val whiteouts = mutable.Set[K]()
+
+  override def put(key: K, value: V): Option[V] = {
+    val e = findOrAddEntry(key, value)
+    if (whiteouts contains key) whiteouts -= key
+    if (e eq null) None
+    else {
+      val v = e.value
+      e.value = value
+      Some(v)
+    }
+  }
+
+  override def get(key: K): Option[V] = {
+    if (whiteouts contains key) None
+    val e = findEntry(key)
+    if (e eq null) {
+      if (parent != null) parent.get(key)
+      else None
+    }
+    else Some(e.value)
+  }
+
+}
