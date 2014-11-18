9c8a117f06e64b60338485c6820303d5130ad9b7
Hawk Weisman <hawk@meteorcodelabs.com>
2014-11-18 12:12:05 -0500

    Remove Scope trait (fine, this doesn't work)

--- src/main/java/com/meteorcode/common/ForkTable.scala
+++ src/main/java/com/meteorcode/common/ForkTable.scala
@@ -8,7 +8,9 @@ import scala.collection.{AbstractMap, DefaultMap, mutable}
  *
  * Created by hawk on 10/15/14.
  */
-class ForkTable[K, V](override var parent: Option[ForkTable[K, V]] = None) extends AbstractMap[K, V] with DefaultMap[K, V] with Scope {
+
+class ForkTable[K, V](protected var parent: ForkTable[K,V] = null,
+                      protected var children: List[ForkTable[K,V]] = Nil) extends AbstractMap[K, V] with DefaultMap[K, V]{
   val whiteouts = mutable.Set[K]()
   val back = mutable.HashMap[K, V]()
 
@@ -17,13 +19,48 @@ class ForkTable[K, V](override var parent: Option[ForkTable[K, V]] = None) exten
     back.put(key, value)
   }
 
+  /**
+   * @return true if this is the root-level, false if it is not
+   */
+  def root: Boolean = parent == null
+  /**
+   * @return true if this is a bottom-level leaf, false if it is not
+   */
+  def leaf: Boolean = parent != null && (children isEmpty)
+  def getParent = parent
+  def getChildren = children
+
+  private def removeChild(other: ForkTable[K, V]): Unit = {
+    this.children = children.filter({other != _})
+  }
+
+  private def addChild(other: ForkTable[K, V]): Unit = {
+    this.children = this.children :+ other
+  }
+
+  /**
+   * Change the parent corresponding to this scope.
+   * @param nParent the new parent
+   * @throws IllegalArgumentException if the specified parent was invalid
+   */
+  @throws[IllegalArgumentException]("if the specified parent was invalid")
+  def reparent(nParent: ForkTable[K, V]) = if (nParent == this) {
+    throw new IllegalArgumentException ("Scope attempted to mount itself as parent!")
+  } else {
+    if (this.parent != null) {
+      val oldParent = this.parent
+      oldParent.removeChild (this)
+    }
+    nParent.addChild (this)
+  }
+
 
   override def get(key: K): Option[V] = if (whiteouts contains key) {
     None
   } else if (this.contains(key)) {
     back get key
-  } else if(parent.isDefined && (parent.get chainContains key)) {
-    parent.get get key
+  } else if(parent != null && (parent chainContains key)) {
+    parent get key
   } else {
     None
   }
@@ -32,9 +69,9 @@ class ForkTable[K, V](override var parent: Option[ForkTable[K, V]] = None) exten
     if (back contains key) {
       back remove key
     } else {
-      if (parent.isDefined && (parent.get contains key)) {
+      if (parent != null && (parent contains key)) {
         whiteouts += key
-        parent.get get key
+        parent get key
       } else {
         None
       }
@@ -48,14 +85,21 @@ class ForkTable[K, V](override var parent: Option[ForkTable[K, V]] = None) exten
    * @param key the key to search for
    * @return true if this or any of its' parents contains the selected key.
    */
-  def chainContains(key: K): Boolean = (back contains key) || ((!(whiteouts contains key)) && parent.isDefined && (parent.get chainContains key))
+  def chainContains(key: K): Boolean = (back contains key) || ((!(whiteouts contains key)) && parent != null && (parent chainContains key))
 
   override def contains(key: K): Boolean = back contains key
   override def exists(p: ((K, V)) => Boolean) = back exists p
 
   override def apply(key: K) = back(key)
 
-  override def fork: ForkTable[K, V] = new ForkTable[K, V](Some(this))
+  /**
+   * @return a new child of this scope
+   */
+  def fork: ForkTable[K, V] = {
+    val c = new ForkTable[K, V](parent=this)
+    children = children :+ c
+    c
+  }
 
   override def toString = this.prettyPrint(0)
 
