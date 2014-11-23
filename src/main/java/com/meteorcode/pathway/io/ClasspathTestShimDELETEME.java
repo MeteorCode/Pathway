c8227945f579217319dda93eb042d58f8ec76ccc
Hawk Weisman <hawk@meteorcodelabs.com>
2014-11-23 16:38:43 -0500

    Remove non-pages stuff from pages branch

--- src/main/java/com/meteorcode/pathway/io/ClasspathTestShimDELETEME.java
+++
@@ -1,7 +0,0 @@
-package com.meteorcode.pathway.io;
-
-public class ClasspathTestShimDELETEME {
-	/*public static ClasspathFileHandle c(String path, ResourceManager manager) {
-		return new ClasspathFileHandle(path, manager);
-	}*/
-}
