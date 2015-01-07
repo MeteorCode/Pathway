de20a1cb59b91bf96b057349f11f66039f8149ed
Hawk Weisman <hawk@meteorcodelabs.com>
2015-01-07 11:43:31 -0500

    Refactor the terrible mess that was Entity

--- src/main/java/com/meteorcode/pathway/model/Entity.scala
+++ src/main/java/com/meteorcode/pathway/model/Entity.scala
@@ -2,50 +2,34 @@ package com.meteorcode.pathway.model
 
 import GridCoordinatesImplicits._
 
-case class Entity(gameID: Option[Long]) extends GameObject(gameID) {
-  protected var coordinates: (Int, Int) = _
-  protected var grid: Grid = _
-  protected var name: String = _
+class Entity(
+              protected var gameID: Option[Long],
+              protected var coordinates: Option[(Int,Int)],
+              protected var name: Option[String],
+              protected var grid: Option[Grid]
+                   ) extends GameObject(gameID) {
 
-  def this() = {
-    this(None)
-    this.name = this.getClass.getSimpleName
-  }
-  def this(gameID: Long) = {
-    this(Some(gameID))
-    this.name = this.getClass.getSimpleName
-  }
-  def this(name: String) = {
-    this(None)
-    this.name = name
-  }
-  def this(gameID: Long, name:String) = {
-    this(Some(gameID))
-    this.name = name
-  }
-  def this(coords: (Int, Int), grid: Grid) = {
-    this(None)
-    this.coordinates = coords
-    this.grid = grid
-    this.name = this.getClass.getSimpleName
-  }
-  def this(coords: (Int, Int), grid: Grid, name: String) = {
-    this(None)
-    this.coordinates = coords
-    this.grid = grid
-    this.name = name
-  }
-  def this(gameID: Long, coords: (Int, Int), grid: Grid, name: String) = {
-    this(Some(gameID))
-    this.coordinates = coordinates
-    this.grid = grid
-    this.name = name
+  def this() = this(None, None, None, None)
+  def this(name: String) = this (None, None, Some(name), None)
+  def this(gameID: Long, name: String) = this(Some(gameID), None, Some(name), None)
+  def this(coords: (Int, Int), grid: Grid) = this(None, Some(coords),None,Some(grid))
+  def this(coords: (Int, Int), grid: Grid, name: String) = this(None, Some(coords),Some(name),Some(grid))
+  def this(gameID: Long, coords: (Int, Int), grid: Grid, name: String) = this(Some(gameID), Some(coords),Some(name),Some(grid))
+
+  name match {
+    case None => name = Some(this.getClass.getSimpleName)
   }
 
-  def setLocation (newCoords: GridCoordinates) = this.coordinates = newCoords
-  def getCoordinates: GridCoordinates = coordinates
-  def getLocation: Tile = grid.getTileAt(coordinates)
-  def getName = new String(name)
-  override def toString = s"Entity $name at $coordinates"
+  def setLocation (newCoords: GridCoordinates) = this.coordinates = Some(newCoords)
+  def getCoordinates: GridCoordinates = coordinates match {
+    case Some(thing) => thing
+    case None => null
+  }
+  def getLocation: Tile = grid match {
+    case Some(g) => g.getTileAt(getCoordinates)
+    case None => null
+  }
+  def getName = new String(name.getOrElse("NamelessEntity (this shouldn't happen)"))
+  override def toString = s"Entity $name ${coordinates.map(c => s"at $c").getOrElse("")}"
 
 }
