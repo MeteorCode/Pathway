eed758b168372b588e143d8af85ebe63427299a9
Hawk Weisman <hawk@meteorcodelabs.com>
2015-05-26 13:32:23 -0400

    Start on spec for Script

---
+++ src/test/java/com/meteorcode/pathway/test/ScriptSpec.scala
@@ -0,0 +1,139 @@
+package com.meteorcode.pathway.test
+
+
+import java.io.IOException
+
+import bsh.{InterpreterError, EvalError, Interpreter}
+import com.meteorcode.pathway.io.FileHandle
+
+import com.meteorcode.pathway.script.{ScriptException, ScriptContainerFactory, ScriptEnvironment}
+
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.prop.PropertyChecks
+import org.scalatest.{Matchers, WordSpec}
+
+import org.mockito.Mockito._
+
+/**
+ * Created by hawk on 5/25/15.
+ */
+class ScriptSpec extends WordSpec with Matchers with PropertyChecks with MockitoSugar {
+
+  "A ScriptContainerFactory" when {
+    "creating a new ScriptContainer with a specified environment" should {
+      "link the new instance with the environment" in {
+        val environment = mock[ScriptEnvironment]
+        val container = new ScriptContainerFactory().getNewInstanceWithEnvironment(environment)
+        verify(environment, times(1)).link(container)
+      }
+    }
+    "creating a new ScriptContainer with a different Interpreter" should {
+      "use the specified Interpreter rather than the default" in {
+        val fakeInterpreter = mock[Interpreter]
+        val container = new ScriptContainerFactory(fakeInterpreter).getNewInstance
+
+        container.eval("")
+
+        verify(fakeInterpreter, times(1)).eval("")
+      }
+    }
+  }
+
+  "A ScriptContainer" when {
+    "evaluating a BeanShell script from a String" should {
+      val fakeInterpreter = mock[Interpreter]
+      val fakeFactory = new ScriptContainerFactory(fakeInterpreter)
+
+      "pass through the result provided by the interpreter" in {
+        forAll { (script: String, result: String) =>
+          whenever(script != "") {
+
+            doReturn(result).when(fakeInterpreter).eval(script)
+            val target = fakeFactory.getNewInstance
+
+            target eval script shouldEqual result
+            verify(fakeInterpreter, times(1)).eval(script)
+          }
+        }
+      }
+      "throw a ScriptException if the Interpreter throws an EvalError" in {
+        forAll { (script: String) =>
+          whenever (script != "") {
+            when(fakeInterpreter.eval(script)) thenThrow new EvalError(null,null,null)
+
+            val target = fakeFactory.getNewInstance
+
+            val e = the [ScriptException] thrownBy target.eval(script)
+            e.getCause shouldBe an [EvalError]
+          }
+        }
+      }
+      "throw a ScriptException if the Interpreter throws an InterpreterError" in {
+        forAll { (script: String) =>
+          whenever (script != "") {
+            when(fakeInterpreter.eval(script)) thenThrow new InterpreterError("fake interp. error")
+
+            val target = fakeFactory.getNewInstance
+
+            val e = the [ScriptException] thrownBy target.eval(script)
+            e.getCause shouldBe an [InterpreterError]
+          }
+        }
+      }
+    }
+    "evaluating a BeanShell script from a FileHandle" should {
+      val fakeInterpreter = mock[Interpreter]
+      val fakeFactory = new ScriptContainerFactory(fakeInterpreter)
+      val fakeFileHandle = mock[FileHandle]
+      "pass through the result provided by the interpreter" in {
+        forAll { (script: String, result: String) =>
+          whenever(script != "") {
+            when(fakeFileHandle.readString) thenReturn script
+            doReturn(result).when(fakeInterpreter).eval(script)
+
+            val target  = fakeFactory.getNewInstance
+
+            target eval fakeFileHandle shouldEqual result
+
+            verify(fakeInterpreter, times(1)).eval(script)
+            verify(fakeFileHandle, times(1)).readString
+          }
+        }
+      }
+      "pass through any IOExceptions thrown by the FileHandle" in {
+        when(fakeFileHandle.readString) thenThrow new IOException
+
+        val target = fakeFactory.getNewInstance
+
+        a [ScriptException] should be thrownBy target.eval(fakeFileHandle)
+      }
+      "throw a ScriptException if the Interpreter throws an EvalError" in {
+        forAll { (script: String) =>
+          whenever (script != "") {
+            when(fakeFileHandle.readString) thenReturn script
+            when(fakeInterpreter.eval(script)) thenThrow new EvalError(null,null,null)
+
+            val target = fakeFactory.getNewInstance
+
+            val e = the [ScriptException] thrownBy target.eval(fakeFileHandle)
+            e.getCause shouldBe an [EvalError]
+          }
+        }
+      }
+      "throw a ScriptException if the Interpreter throws an InterpreterError" in {
+        forAll { (script: String) =>
+          whenever (script != "") {
+            when(fakeFileHandle.readString) thenReturn script
+            when(fakeInterpreter.eval(script)) thenThrow new InterpreterError("fake interp. error")
+
+            val target = fakeFactory.getNewInstance
+
+            val e = the [ScriptException] thrownBy target.eval(fakeFileHandle)
+            e.getCause shouldBe an [InterpreterError]
+          }
+        }
+      }
+
+    }
+  }
+}
