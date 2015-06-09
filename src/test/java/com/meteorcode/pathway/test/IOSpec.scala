7fa1647887baea1272c1fa922c226f8cb0ced1bc
Hawk Weisman <hawk@meteorcodelabs.com>
2015-06-09 13:32:25 -0400

    Re-added tests that were removed for pineapple reasons.
    I had forgotten that two ignored tests were removed when trying to fix the CI build issues in 106b9202924be0d236aea6ef506a304df844639a. Since the build on CI was fixed in 4744653f4bee2824f6bb0c28284625c0181cd50e, there's no reason for these tests to still be deleted.
    
    This commit:
    
      + Adds back in ignored test for ForkTable.freeze()
      + Adds back in and un-ignores test for JarFileHandle.write()
      + Removes pineapple
      + Finally gets back down off of the chair.

--- src/test/java/com/meteorcode/pathway/test/IOSpec.scala
+++ src/test/java/com/meteorcode/pathway/test/IOSpec.scala
@@ -317,6 +317,9 @@ class IOSpec extends PathwaySpec {
       "return null from calls to write() in append mode" taggedAs FilesystemTest in {
         manager.handle("/testJarDir/").write(append=true) shouldBe null
       }
+      "return null from calls to write() in append mode with a specified buffer size" taggedAs FilesystemTest in {
+        manager.handle("/testJarDir").write(8,append=true) shouldBe null
+      }
       "return null from calls to write() in overwrite mode" taggedAs FilesystemTest in {
         manager.handle("/testJarDir").write(append=false) shouldBe null
       }
