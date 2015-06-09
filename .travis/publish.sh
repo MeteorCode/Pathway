25c5b98fea393f00034e7f20a1f3b5809f96a6dc
Hawk Weisman <hi@hawkweisman.me>
2015-06-09 14:07:28 -0400

    Remove conditional from release script [skip-ci]
    This can be expressed much easier by just putting the publish script in Travis' `after_deploy` block, and then we won't have issues with it not knowing to publish.

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
+echo -e "Publishing javadoc...\n"
 
-if [ "$TRAVIS_REPO_SLUG" == "MeteorCode/Pathway" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+cp -R build/docs/javadoc $HOME/javadoc-latest
 
-	echo -e "Publishing javadoc...\n"
+cd $HOME
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
+git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/MeteorCode/pathway gh-pages > /dev/null
 
-	cp -R build/docs/javadoc $HOME/javadoc-latest
+cd gh-pages
+git rm -rf ./
+cp -Rf $HOME/javadoc-latest .
+git add -f .
+git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+git push -fq origin gh-pages > /dev/null
 
-	cd $HOME
-	git config --global user.email "travis@travis-ci.org"
-	git config --global user.name "travis-ci"
-	git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/MeteorCode/pathway gh-pages > /dev/null
+echo -e "Published Javadoc to gh-pages.\n"
 
-	cd gh-pages
-	git rm -rf ./
-	cp -Rf $HOME/javadoc-latest .
-	git add -f .
-	git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-	git push -fq origin gh-pages > /dev/null
-
-	echo -e "Published Javadoc to gh-pages.\n"
-
-fi
