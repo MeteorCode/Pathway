33c19ac81161d63f0638e958ef74390197489b39
Hawk Weisman <hawk@meteorcodelabs.com>
2015-06-09 11:24:05 -0400

    Added documentation to ForkTable

--- src/main/java/com/meteorcode/common/ForkTable.scala
+++ src/main/java/com/meteorcode/common/ForkTable.scala
@@ -3,8 +3,24 @@ package com.meteorcode.common
 import scala.collection.{AbstractMap, DefaultMap, mutable}
 
 /**
- * Scala re-implementation of Max's ClobberTable
+ * An associative map data structure for representing scopes.
  *
+ * A `ForkTable` functions similarly to a standard associative map
+ * data structure (such as a [[HashMap]]), but with the ability to
+ * fork children off of each level of the map. If a key exists in any
+ * of a child's parents, the child will 'pass through' that key. If a
+ * new value is bound to a key in a child level, that child will overwrite
+ * the previous entry with the new one, but the previous `key` -> `value`
+ * mapping will remain in the level it is defined. This means that the parent
+ * level will still provide the previous value for that key.
+ *
+ * This implementation of the `ForkTable` is based on one originally developed
+ * for a compiler for the Decaf programming language. The data structure
+ * was originally described by Max Clive.
+ *
+ * @author Hawk Weisman
+ * @author Max Clive
+ * @since v1.0.2
  *
  * Created by hawk on 10/15/14.
  */
@@ -14,6 +30,22 @@ class ForkTable[K, V](protected var parent: Option[ForkTable[K,V]] = None,
   val whiteouts = mutable.Set[K]()
   val back = mutable.HashMap[K, V]()
 
+  /**
+   * Inserts a key-value pair from the map.
+   *
+   * If the key already had a value present in the map, that
+   * value is returned. Otherwise, [[scala.None None]] is returned.
+   *
+   * If the key is currently whited out (i.e. it was defined
+   * in a lower level of the map and was removed) then it will
+   * be un-whited out and added at this level.
+   *
+   * @param key a key
+   * @param value the value to associate with the key
+   * @return an [[scala.Option Option]] containing the previous
+   *         value associated with that key, or [[scala.None None]]
+   *         if the key was undefined
+   */
   def put(key: K, value: V): Option[V] = {
     if (whiteouts contains key) whiteouts -= key
     back.put(key, value)
@@ -27,7 +59,16 @@ class ForkTable[K, V](protected var parent: Option[ForkTable[K,V]] = None,
    * @return true if this is a bottom-level leaf, false if it is not
    */
   def leaf: Boolean = parent.isDefined && (children.isEmpty)
+  /**
+   * @return an [[scala.Option Option]] containing a reference to
+   *         the parent table, or [[scala.None None]] if this is
+   *         the root level of the tree
+   */
   def getParent: Option[ForkTable[K,V]] = parent
+
+  /**
+   * @return a sequence of this level's child ForkTables.
+   */
   def getChildren: Seq[ForkTable[K,V]] = children
 
   private def removeChild(other: ForkTable[K, V]): Unit = {
@@ -38,6 +79,9 @@ class ForkTable[K, V](protected var parent: Option[ForkTable[K,V]] = None,
     this.children = this.children :+ other
   }
 
+  /**
+   * @return the number of keys defined in this level plus all previous levels
+   */
   def chainSize: Int =  size + (parent map (_.chainSize) getOrElse 0)
 
   /**
@@ -54,12 +98,34 @@ class ForkTable[K, V](protected var parent: Option[ForkTable[K,V]] = None,
     nParent.addChild (this)
   }
 
+  /**
+   * Returns the value corresponding to the given key.
+   *
+   * @param  key the key to look up
+   * @return a [[scala.Option Option]] containing the value of the
+   *         key, or [[scala.None None]] if it is undefined.
+   */
   override def get(key: K): Option[V] = if (whiteouts contains key) {
     None
   } else {
     (back get key) orElse (parent flatMap (_ get key ))
   }
 
+  /**
+   * Removes a binding from the map and returns the value
+   * corresponding to the given key.
+   *
+   * If the removed value exists in a lower level of the table,
+   * it will be whited out at this level. This means that the entry
+   * will be 'removed' at this level and this table will not provide
+   * access to it, but the mapping will still exist in the level where
+   * it was defined. Note that the key will not be returned if it is
+   * defined in a lower level of the table.
+   *
+   * @param  key the key to remove.
+   * @return a [[scala.Option Option]] containing the value of the
+   *         key, or [[scala.None None]] if it is undefined.
+   */
   def remove(key: K): Option[V] = if (back contains key) {
     back remove key
   } else {
@@ -68,8 +134,12 @@ class ForkTable[K, V](protected var parent: Option[ForkTable[K,V]] = None,
 
   def freeze: Unit = ???
 
+  /** @return the number of entries in this level over the table.
+   */
   override def size: Int = back.size
 
+  /** @return an Iterator over all of the (key, value) pairs in the tree.
+   */
   override def iterator: Iterator[(K,V)] = parent match {
     case None        => back.iterator
     case Some(thing) => back.iterator ++ thing.iterator
@@ -83,12 +153,35 @@ class ForkTable[K, V](protected var parent: Option[ForkTable[K,V]] = None,
   def chainContains(key: K): Boolean = (back contains key) ||
   ( !(whiteouts contains key) && (parent map (_ chainContains key) getOrElse false) )
 
+  /**
+   * @param  key the key to look up
+   * @return true if this level contains a binding for the given key, false otherwise
+   */
   override def contains(key: K): Boolean = back contains key
+
+  /**
+   * Search this level for a (key, value) pair matching a predicate.
+   *
+   * @param  p the predicate to search for
+   * @return true if there exists a pair defined at this level for
+   *         which the predicate holds, false otherwise.
+   */
   override def exists(p: ((K, V)) => Boolean) = back exists p
 
+  /**
+   * Look up the given key
+   * @param  key the key to look up
+   * @return the value bound to that key.
+   */
   override def apply(key: K): V = back(key)
 
   /**
+   * Forks this table, returning a new `ForkTable[K,V]`.
+   *
+   * This level of the table will be set as the child's
+   * parent. The child will be created with an empty backing
+   * [[scala.collection.HashMap HashMap]] and no keys whited out.
+   *
    * @return a new child of this scope
    */
   def fork(): ForkTable[K, V] = {
@@ -97,7 +190,15 @@ class ForkTable[K, V](protected var parent: Option[ForkTable[K,V]] = None,
     c
   }
 
+ /**
+  * @return a String representation of this ForkTable
+  */
   override def toString: String = this.prettyPrint(0)
 
+  /**
+   * Helper method for printing indented levels of a ForkTable
+   * @param indentLevel the level to indent to
+   * @return a String representing this table indented at the specified level
+   */
   def prettyPrint(indentLevel: Int): String = (" "*indentLevel) + this.keys.foldLeft[String](""){(acc, key) => acc + "\n" + (" " * indentLevel) + s"$key ==> ${this.get(key).getOrElse("")}"}
 }
