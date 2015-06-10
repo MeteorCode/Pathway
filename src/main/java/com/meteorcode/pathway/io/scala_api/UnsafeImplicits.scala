257653c39b6c8a5baed2e57682d6282ff41abc30
Hawk Weisman <hawk@meteorcodelabs.com>
2015-06-10 17:44:27 -0400

    Add implicit FileHandle -> Source conversion

---
+++ src/main/java/com/meteorcode/pathway/io/scala_api/UnsafeImplicits.scala
@@ -0,0 +1,23 @@
+package com.meteorcode.pathway.io.scala_api
+
+import java.io.IOException
+
+import scala.language.implicitConversions
+import scala.io.Source
+
+/**
+ * Created by hawk on 6/10/15.
+ */
+object UnsafeImplicits {
+
+  /**
+   * Implicitly converts a [[FileHandle]] to a [[scala.io.Source]], throwing
+   * any exceptions produced when reading the file. Only use this when you know
+   * files will be readable, or while wrapped in a Try block.
+   * @param f a [[FileHandle]]
+   * @return a [[scala.io.Source]]
+   */
+  @throws(classOf[IOException])
+  implicit def filehandleAsSource(f: FileHandle): Source = Source fromInputStream f.read.get
+
+}
