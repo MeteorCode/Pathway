f9a98b04e870e23af006c9f7a461032b006de772
Hawk Weisman <hawk@meteorcodelabs.com>
2015-06-10 18:06:26 -0400

    Java FileHandles can now be used for creating a ResourceManager

--- src/main/java/com/meteorcode/pathway/io/java_api/FileHandle.scala
+++ src/main/java/com/meteorcode/pathway/io/java_api/FileHandle.scala
@@ -7,10 +7,12 @@ import scala.language.implicitConversions
 /**
  * Created by hawk on 6/10/15.
  */
-class FileHandle protected[io] (private[this] val underlying: scala_api.FileHandle) {
+class FileHandle protected[io] (private val underlying: scala_api.FileHandle) {
+
 
 }
 
 object FileHandle {
-  implicit def fromScalaHandle(handle: scala_api.FileHandle): FileHandle = new FileHandle(handle)
+  implicit def fromScala(handle: scala_api.FileHandle): FileHandle = new FileHandle(handle)
+  implicit def asScala(handle: FileHandle): scala_api.FileHandle = handle.underlying
 }
