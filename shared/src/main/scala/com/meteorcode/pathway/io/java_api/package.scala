230817ea38ac546542c3e24cc6ae56884e68be40
Hawk Weisman <hi@hawkweisman.me>
2015-07-05 16:48:45 -0400

    Java API code quality improvements
    + Make implicit more elegant
    + Wrap all lines at 80th column
    + Remove calls to `Try.get` using `fold` (using the shim from
    me.hawkweisman.util until Scala 2.12 releases)

--- shared/src/main/scala/com/meteorcode/pathway/io/java_api/package.scala
+++ shared/src/main/scala/com/meteorcode/pathway/io/java_api/package.scala
@@ -5,10 +5,27 @@ import java.util.Optional
 import scala.language.implicitConversions
 
 /**
+ * Pathway IO Java API
+ * -------------------
+ *
+ * This is a version of the Pathway input/output API to be used by Java
+ * clients.
+ *
+ * @author Hawk Weisman
  * Created by hawk on 6/10/15.
  */
 package object java_api {
-  implicit def optionToOptional[T](option: Option[T]): Optional[T] = option map
-    (value => Optional.of(value)) getOrElse Optional.empty()
+  /**
+   * Implicitly transform a [[scala.Option Option]] into a
+   * [[java.util.Optional]].
+   *
+   * This really ought to be in the Scala standard library.
+   *
+   * @param  option an [[scala.Option]]
+   * @return an [[java.util.Optional]] containing either the contents
+   *         of that Option if it is Some, or nothing if it is None.
+   */
+  implicit def optionToOptional[T >: Null](option: Option[T]): Optional[T]
+    = Optional ofNullable (option orNull)
 
 }
