80255ad9ec1b4c4a398e09ba50ce2b3609b5ccc2
Hawk Weisman <hi@hawkweisman.me>
2015-08-11 21:49:06 -0400

    Prepare Unpacker to merge in   + Changed natives location to the one SBT puts things into currently   + Log error messages using the system logger   + Tried to make code slightly less ugly.

--- src/main/scala/me/arcticlight/tempo/reswizard/Unpacker.scala
+++ src/main/scala/me/arcticlight/tempo/reswizard/Unpacker.scala
@@ -4,7 +4,11 @@ import java.io.{File, IOException}
 import java.nio.file._
 import java.nio.file.attribute.BasicFileAttributes
 import java.net.{URL,URI}
+
+import com.typesafe.scalalogging.LazyLogging
+
 import collection.JavaConverters._
+import scala.util.{Success, Try}
 import scala.util.control.NonFatal
 
 /**
@@ -14,60 +18,81 @@ import scala.util.control.NonFatal
  * Native dependencies such as LWJGL's natives before running code
  * that requires it.
  */
-object Unpacker {
-  val separator = System.getProperty("file.separator")
-  val defaultLocation = System.getProperty("user.home") + separator + ".pathway" + separator
+object Unpacker
+extends LazyLogging {
+  val sep = System.getProperty("file.separator")
+  val defaultLocation = System.getProperty("user.home") + s"$sep.pathway$sep"
 
   /**
-   * Attempts to unpack native JARs into the host filesystem, because JNI requires this.
-   * @return True if and only if the unpacking succeeds, and the classpath is edited to include the new natives directory. False otherwise.
+   * Attempts to unpack native JARs into the host filesystem, because JNI
+   * requires this.
+   * @return True if and only if the unpacking succeeds, and the classpath
+   *         is edited to include the new natives directory. False otherwise.
    */
-  def unpackNatives(destLocation:String = defaultLocation, srcURL:URL = this.getClass.getProtectionDomain.getCodeSource.getLocation):Boolean = {
+  def unpackNatives(destLocation: String = defaultLocation,
+                    srcURL:URL = this.getClass
+                                     .getProtectionDomain
+                                     .getCodeSource
+                                     .getLocation): Boolean = {
+
     val targetDir = Paths.get(destLocation)
 
     //Add destLocation/native to the classloader via an ugly hack
-    me.arcticlight.tempo.reswizard.UnpackerJavaCallouts.mangleClassloader(Paths.get(destLocation).resolve("native").toString)
+    UnpackerJavaCallouts.mangleClassloader(
+      Paths.get(destLocation)
+           .resolve("native")
+           .toString
+      )
 
     //We cannot unpack if the target location is read only.
     if(targetDir.getFileSystem.isReadOnly) return false;
 
-    val zURI = URI.create("jar:" + srcURL.toURI.toString + "!/buildres")
+    val zURI = URI.create("jar:" + srcURL.toURI.toString + "!/lwjgl-natives")
     try {
       FileSystems.newFileSystem(zURI, Map("create" -> "false").asJava)
     } catch {
       case x: FileSystemAlreadyExistsException =>
-      case x if NonFatal(x) => System.err.println(s"Warning: ${x.getMessage}")
+      case x if NonFatal(x) =>
+        logger.warn("An exception occurred while creating FileSystem", x)
     }
 
     val top = Paths.get(zURI)
-    Files.walkFileTree(top, new FileVisitor[Path] {
+    Files.walkFileTree(top, new FileVisitor[Path] with LazyLogging {
       import FileVisitResult._
 
-      override def preVisitDirectory(dir: Path, attrs: BasicFileAttributes): FileVisitResult = {
-        if(dir.toString.compareTo(top.toString) != 0) {
-          try {
-            Files.createDirectory(targetDir.resolve(top.relativize(dir).toString))
-          } catch {
-            case x: FileAlreadyExistsException =>
-            case x if NonFatal(x) => System.err.println(s"Warning: ${x.getMessage}")
+      override def preVisitDirectory(dir: Path,attrs: BasicFileAttributes): FileVisitResult
+        = { if(dir.toString.compareTo(top.toString) != 0) {
+              try {
+                Files.createDirectory(
+                  targetDir.resolve(top.relativize(dir).toString)
+                )
+              } catch {
+                case x: FileAlreadyExistsException =>
+                case x if NonFatal(x) =>
+                  logger.warn(s"An exception occurred before visiting $dir", x)
+              }
+            }
+          CONTINUE
+        }
+
+      override def visitFileFailed(file: Path, exc: IOException): FileVisitResult
+        = CONTINUE
+
+      override def postVisitDirectory(dir: Path, exc: IOException): FileVisitResult
+        = CONTINUE
+
+      override def visitFile(file: Path, attrs: BasicFileAttributes): FileVisitResult
+        = { try {
+              Files.copy(Files.newInputStream(file),
+                         targetDir.resolve(top.relativize(file).toString)
+                )
+            } catch {
+              case x: FileAlreadyExistsException =>
+              case x if NonFatal(x) =>
+                logger.warn(s"An exception occurred while visiting $file", x)
+            }
+            CONTINUE
           }
-        }
-
-        CONTINUE
-      }
-
-      override def visitFileFailed(file: Path, exc: IOException): FileVisitResult = CONTINUE
-      override def postVisitDirectory(dir: Path, exc: IOException): FileVisitResult = CONTINUE
-
-      override def visitFile(file: Path, attrs: BasicFileAttributes): FileVisitResult = {
-        try {
-          Files.copy(Files.newInputStream(file), targetDir.resolve(top.relativize(file).toString))
-        } catch {
-          case x: FileAlreadyExistsException =>
-          case x if NonFatal(x) => System.err.println(s"Warning: ${x.getMessage}")
-        }
-        CONTINUE
-      }
     })
     return true
   }
