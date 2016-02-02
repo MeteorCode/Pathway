ee34b93fae2610b997b869985239cbe8905dbe9c
Hawk Weisman <hi@hawkweisman.me>
2016-02-02 16:11:28 -0500

    [io] Fix issue where Path.name was extracted improperly

--- src/main/scala/com/meteorcode/pathway/io/package.scala
+++ src/main/scala/com/meteorcode/pathway/io/package.scala
@@ -37,22 +37,22 @@ package object io {
 
   protected[io] implicit class Path(val path: String) {
 
-    @inline def extension: Option[String]
-      = path split '.'  drop 1 lastOption
+    lazy val extension: Option[String]
+      = path split '.' drop 1 lastOption
 
-    @inline def name: Option[String]
-      = lastPathEntry flatMap {  _ split '.' headOption  }
+    lazy val name: Option[String]
+      = path.split('/').lastOption flatMap {  _ split '.' headOption }
 
     /**
       * Removes the trailing slash from a path.
  *
       * @return the path with any trailing slashes removed.
       */
-    @inline def withoutTrailingSlash: String
-      = if (path endsWith "/") path dropRight 1 else path
+    lazy val withoutTrailingSlash: String
+      = if (path endsWith "/") { path dropRight 1 } else { path }
 
-    @inline def lastPathEntry: Option[String]
-      = path split "/" dropRight 1 lastOption
+    lazy val lastPathEntry: Option[String]
+      = path split '/' dropRight 1 lastOption
   }
 
   // Regex for determining if a path is inside an archive
