0cc9cec6d5c444481179c74473ce61730c11f6b0
Hawk Weisman <hi@hawkweisman.me>
2016-02-02 15:51:26 -0500

    [io] Add PathNormalizations trait

---
+++ src/main/scala/com/meteorcode/pathway/io/PathNormalizations.scala
@@ -0,0 +1,39 @@
+package com.meteorcode.pathway.io
+
+import org.scalactic.{AbstractStringUniformity, Uniformity}
+
+import scala.language.postfixOps
+
+/**
+  * Scalactic normalizations for strings that represent paths
+  *
+  * Created by hawk on 2/2/16.
+  */
+trait PathNormalizations {
+
+  /**
+    * Produces a <code>Uniformity[String]</code> whose <code>normalized</code>
+    * method removes the trailing slash
+    *
+    * @return a <code>Uniformity[String]</code> that normalizes by
+    *         pruning the trailing slash
+    */
+  val trailingSlashNormed: Uniformity[String]
+    = new AbstractStringUniformity {
+
+        def normalized(s: String): String
+          = if (s endsWith "/") s dropRight 1 else s
+
+        override def toString: String = "trailingSlashNormed"
+      }
+
+  val extensionTrimmed: Uniformity[String]
+    = new AbstractStringUniformity {
+
+      def normalized(s: String): String
+        = s split '.' dropRight 1 mkString "."
+
+      override def toString: String = "extensionTrimmed"
+    }
+
+}
